e74790f91ad706d2c8f310592e3c76f5bb58ac7d
siddheshkale2003 <112115154@cse.iiitp.ac.in>
2025-04-23 01:25:50 +0530

    consumer

---
+++ src/main/java/com/example/AuthenticationService/service/KafkaMessageListener.java
@@ -0,0 +1,18 @@
+package com.example.AuthenticationService.service;
+
+import com.example.AuthenticationService.dto.UserSyncDto;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.util.logging.Logger;
+
+@Service
+public class KafkaMessageListener {
+
+
+    @KafkaListener(topics = "profile", groupId = "jt-group")
+    public void consumeEvents(UserSyncDto userSyncDto) {
+        System.out.println(userSyncDto.toString());
+    }
+}
